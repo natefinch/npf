9c59b6daa97f28b897cee6d17855234ab4564a6e
Roger Peppe <rogpeppe@gmail.com>
2014-07-18 11:44:20 +0100

    internal/...: create new structure for api

---
+++ internal/charmstore/server_test.go
@@ -0,0 +1,96 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the LGPLv3, see LICENCE file for details.
+
+package charmstore
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/juju/charmstore/internal/router"
+	"github.com/juju/charmstore/internal/storetesting"
+	jujutesting "github.com/juju/testing"
+	gc "launchpad.net/gocheck"
+)
+
+func TestPackage(t *testing.T) {
+	jujutesting.MgoTestPackage(t, nil)
+}
+
+type ServerSuite struct {
+	storetesting.IsolatedMgoSuite
+}
+
+var _ = gc.Suite(&ServerSuite{})
+
+func (s *ServerSuite) TearDownTest(c *gc.C) {
+	s.IsolatedMgoSuite.TearDownTest(c)
+	versions = make(map[string]func(*Store) http.Handler)
+}
+
+func (s *ServerSuite) TestNewServerWithNoVersions(c *gc.C) {
+	h, err := NewServer(s.Session.DB("foo"))
+	c.Assert(err, gc.ErrorMatches, `charm store server must serve at least one version of the API`)
+	c.Assert(h, gc.IsNil)
+}
+
+func (s *ServerSuite) TestNewServerWithUnregisteredVersion(c *gc.C) {
+	h, err := NewServer(s.Session.DB("foo"), "wrong")
+	c.Assert(err, gc.ErrorMatches, `API version "wrong" not registered`)
+	c.Assert(h, gc.IsNil)
+}
+
+type versionResponse struct {
+	Version string
+	Path    string
+}
+
+func (s *ServerSuite) TestNewServerWithVersions(c *gc.C) {
+	db := s.Session.DB("foo")
+	serveVersion := func(vers string) func(store *Store) http.Handler {
+		return func(store *Store) http.Handler {
+			c.Assert(store.DB(), gc.Equals, db)
+			return router.HandleJSON(func(w http.ResponseWriter, req *http.Request) (interface{}, error) {
+				return versionResponse{
+					Version: vers,
+					Path:    req.URL.Path,
+				}, nil
+			})
+		}
+	}
+	for i := 1; i < 4; i++ {
+		vers := fmt.Sprintf("version%d", i)
+		RegisterAPIVersion(vers, serveVersion(vers))
+	}
+
+	h, err := NewServer(db, "version1")
+	c.Assert(err, gc.IsNil)
+	assertServesVersion(c, h, "version1")
+	assertDoesNotServeVersion(c, h, "version2")
+	assertDoesNotServeVersion(c, h, "version3")
+
+	h, err = NewServer(db, "version1", "version2")
+	c.Assert(err, gc.IsNil)
+	assertServesVersion(c, h, "version1")
+	assertServesVersion(c, h, "version2")
+	assertDoesNotServeVersion(c, h, "version3")
+
+	h, err = NewServer(db, "version1", "version2", "version3")
+	c.Assert(err, gc.IsNil)
+	assertServesVersion(c, h, "version1")
+	assertServesVersion(c, h, "version2")
+	assertServesVersion(c, h, "version3")
+}
+
+func assertServesVersion(c *gc.C, h http.Handler, vers string) {
+	storetesting.AssertJSONCall(c, h, "GET", "http://0.1.2.3/"+vers+"/some/path", "", http.StatusOK, versionResponse{
+		Version: vers,
+		Path:    "/some/path",
+	})
+}
+
+func assertDoesNotServeVersion(c *gc.C, h http.Handler, vers string) {
+	rec := storetesting.DoRequest(c, h, "GET", "http://0.1.2.3/"+vers+"/some/path", "")
+	c.Assert(rec.Code, gc.Equals, http.StatusNotFound)
+}
---
+++ internal/v4/api.go
@@ -0,0 +1,203 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the LGPLv3, see LICENCE file for details.
+
+package v4
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/juju/charmstore/internal/charmstore"
+	"github.com/juju/charmstore/internal/router"
+	"gopkg.in/juju/charm.v2"
+)
+
+func init() {
+	charmstore.RegisterAPIVersion("v4", newHandler)
+}
+
+type handler struct {
+	*router.Router
+	store *charmstore.Store
+}
+
+func newHandler(store *charmstore.Store) http.Handler {
+	h := &handler{
+		store: store,
+	}
+	h.Router = router.New(nil, &router.Handlers{
+		Global: map[string]http.Handler{
+			"stats/counter":      http.HandlerFunc(h.serveStatsCounter),
+			"search":             http.HandlerFunc(h.serveSearch),
+			"search/interesting": http.HandlerFunc(h.serveSearchInteresting),
+			"debug":              http.HandlerFunc(h.serveDebug),
+		},
+		Id: map[string]router.IdHandler{
+			"resources": h.serveResources,
+			"archive":   h.serveArchive,
+			"archive/":  h.serveArchiveFile,
+			"expand-id": h.serveExpandId,
+		},
+		Meta: map[string]router.MetaHandler{
+			"charm-metadata":      h.metaCharmMetadata,
+			"bundle-metadata":     h.metaBundleMetadata,
+			"manifest":            h.metaManifest,
+			"charm-actions":       h.metaCharmActions,
+			"charm-config":        h.metaCharmConfig,
+			"color":               h.metaColor,
+			"archive-size":        h.metaArchiveSize,
+			"bundles-containing":  h.metaBundlesContaining,
+			"extra-info":          h.metaExtraInfo,
+			"extra-info/":         h.metaExtraInfoWithKey,
+			"charm-related":       h.metaCharmRelated,
+			"archive-upload-time": h.metaArchiveUploadTime,
+		},
+	})
+	return h
+}
+
+var errNotImplemented = fmt.Errorf("method not implemented")
+
+// GET stats/counter/key[:key]...?[by=unit]&start=date][&stop=date][&list=1]
+// http://tinyurl.com/nkdovcf
+func (h *handler) serveStatsCounter(w http.ResponseWriter, req *http.Request) {
+	router.WriteError(w, errNotImplemented)
+}
+
+// GET search[?text=text][&autocomplete=1][&filter=value…][&limit=limit][&include=meta]
+// http://tinyurl.com/qzobc69
+func (h *handler) serveSearch(w http.ResponseWriter, req *http.Request) {
+	router.WriteError(w, errNotImplemented)
+}
+
+// GET search/interesting[?limit=limit][&include=meta]
+// http://tinyurl.com/ntmdrg8
+func (h *handler) serveSearchInteresting(w http.ResponseWriter, req *http.Request) {
+	router.WriteError(w, errNotImplemented)
+}
+
+// GET /debug
+// http://tinyurl.com/m63xhz8
+func (h *handler) serveDebug(w http.ResponseWriter, req *http.Request) {
+	router.WriteError(w, errNotImplemented)
+}
+
+// POST id/resources/name.stream
+// http://tinyurl.com/pnmwvy4
+//
+// GET  id/resources/name.stream[-revision]/arch/filename
+// http://tinyurl.com/pydbn3u
+//
+// PUT id/resources/[~user/]series/name.stream-revision/arch?sha256=hash
+// http://tinyurl.com/k8l8kdg
+func (h *handler) serveResources(charmId *charm.URL, w http.ResponseWriter, req *http.Request) error {
+	return errNotImplemented
+}
+
+// GET id/expand-id
+// https://docs.google.com/a/canonical.com/document/d/1TgRA7jW_mmXoKH3JiwBbtPvQu7WiM6XMrz1wSrhTMXw/edit#bookmark=id.4xdnvxphb2si
+func (h *handler) serveExpandId(charmId *charm.URL, w http.ResponseWriter, req *http.Request) error {
+	return errNotImplemented
+}
+
+// GET id/archive
+// http://tinyurl.com/qjrwq53
+//
+// POST id/archive?sha256=hash
+// http://tinyurl.com/lzrzrgb
+func (h *handler) serveArchive(charmId *charm.URL, w http.ResponseWriter, req *http.Request) error {
+	log.Printf("in serveArchive")
+	return errNotImplemented
+}
+
+// GET id/archive/…
+// http://tinyurl.com/lampm24
+func (h *handler) serveArchiveFile(charmId *charm.URL, w http.ResponseWriter, req *http.Request) error {
+	return errNotImplemented
+}
+
+// GET id/meta/charm-metadata
+// http://tinyurl.com/poeoulw
+func (h *handler) metaCharmMetadata(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
+
+// GET id/meta/bundle-metadata
+// http://tinyurl.com/ozshbtb
+func (h *handler) metaBundleMetadata(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
+
+// GET id/meta/manifest
+// http://tinyurl.com/p3xdcto
+func (h *handler) metaManifest(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
+
+// GET id/meta/charm-actions
+// http://tinyurl.com/kfd2h34
+func (h *handler) metaCharmActions(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
+
+// GET id/meta/charm-config
+// http://tinyurl.com/oxxyujx
+func (h *handler) metaCharmConfig(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
+
+// GET id/meta/color
+// http://tinyurl.com/o2t3j4p
+func (h *handler) metaColor(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
+
+// GET id/meta/revision-info
+// http://tinyurl.com/q6xos7f
+func (h *handler) revisionInfo(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
+
+// GET id/meta/archive-size
+// http://tinyurl.com/m8b9geq
+func (h *handler) metaArchiveSize(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
+
+// GET id/meta/stats/
+// http://tinyurl.com/lvyp2l5
+func (h *handler) metaStats(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
+
+// GET id/meta/bundles-containing[?include=meta[&include=meta…]]
+// http://tinyurl.com/oqc386r
+func (h *handler) metaBundlesContaining(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
+
+// GET id/meta/extra-info
+// http://tinyurl.com/keos7wd
+func (h *handler) metaExtraInfo(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
+
+// GET id/meta/extra-info/key
+// http://tinyurl.com/polrbn7
+func (h *handler) metaExtraInfoWithKey(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
+
+// GET id/meta/charm-related[?include=meta[&include=meta…]]
+// http://tinyurl.com/q7vdmzl
+func (h *handler) metaCharmRelated(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
+
+// GET id/meta/archive-upload-time
+// http://tinyurl.com/nmujuqk
+func (h *handler) metaArchiveUploadTime(getter router.ItemGetter, id *charm.URL, path string, flags url.Values) (interface{}, error) {
+	return nil, errNotImplemented
+}
