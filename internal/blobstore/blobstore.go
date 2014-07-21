a2c9ac7dc01078ff5226739b0b5dcb7f3b22140d
Roger Peppe <rogpeppe@gmail.com>
2014-07-21 12:20:10 +0100

    internal/blobstore, internal/multihash: new packages

---
+++ internal/blobstore/blobstore.go
@@ -0,0 +1,136 @@
+package blobstore
+
+import (
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"hash"
+	"io"
+
+	"labix.org/v2/mgo"
+
+	"github.com/juju/blobstore"
+	"github.com/juju/charmstore/internal/multihash"
+	"github.com/juju/errors"
+)
+
+type ReadSeekCloser interface {
+	io.Reader
+	io.Seeker
+	io.Closer
+}
+
+// ContentChallengeError holds a proof-of-content
+// challenge produced by a blobstore.
+type ContentChallengeError struct {
+	Req ContentChallenge
+}
+
+func (e *ContentChallengeError) Error() string {
+	return "cannot upload because proof of content ownership is required"
+}
+
+// ContentChallenge holds a proof-of-content challenge
+// produced by a blobstore. A client can satisfy the request
+// by producing a ContentChallengeResponse containing
+// the same request id and a hash of RangeLength bytes
+// of the content starting at RangeStart.
+type ContentChallenge struct {
+	RequestId   string
+	RangeStart  int64
+	RangeLength int64
+}
+
+// ContentChallengeResponse holds a response to a ContentChallenge.
+type ContentChallengeResponse struct {
+	RequestId string
+	Hash      string
+}
+
+// NewHash is used to calculate checksums for the blob store.
+func NewHash() hash.Hash {
+	return multihash.New(md5.New(), sha256.New())
+}
+
+// Prove can be used by a client to responsd to a content
+// challenge. The returned value should be passed to BlobStorage.Put
+// when the client retries the request.
+func Prove(chal *ContentChallenge, r io.ReadSeeker) (*ContentChallengeResponse, error) {
+	_, err := r.Seek(chal.RangeStart, 0)
+	if err != nil {
+		return nil, err
+	}
+	hash := NewHash()
+	nw, err := io.CopyN(hash, r, chal.RangeLength)
+	if err != nil {
+		return nil, err
+	}
+	if nw != chal.RangeLength {
+		return nil, fmt.Errorf("content is not long enough")
+	}
+	return &ContentChallengeResponse{
+		RequestId: chal.RequestId,
+		Hash:      fmt.Sprintf("%x", hash.Sum(nil)),
+	}, nil
+}
+
+// Store stores data blobs in mongodb.
+type Store struct {
+	mstore blobstore.ManagedStorage
+}
+
+func New(db *mgo.Database, prefix string) *Store {
+	rs := blobstore.NewGridFS(db.Name, prefix, db.Session)
+	return &Store{
+		mstore: blobstore.NewManagedStorage(db, rs),
+	}
+}
+
+// Put tries to stream the content from the given reader into blob
+// storage. The content should have the given size and hash. If the
+// content is already in the store, a ContentChallengeError is returned
+// containing a challenge that must be satisfied by a client to prove
+// that they have access to the content. If the proof has already been
+// acquired, it should be passed in as the proof argument.
+func (s *Store) Put(r io.Reader, size int64, hash string, proof *ContentChallengeResponse) (*ContentChallenge, error) {
+	resp, err := s.mstore.PutForEnvironmentRequest("", hash, *newResourceHash(hash))
+	if err != nil {
+		if errors.IsNotFound(err) {
+			if err := s.mstore.PutForEnvironment("", hash, r, size); err != nil {
+				return nil, err
+			}
+		}
+		return nil, err
+	}
+	return &ContentChallenge{
+		RequestId:   fmt.Sprint(resp.RequestId),
+		RangeStart:  resp.RangeStart,
+		RangeLength: resp.RangeLength,
+	}, nil
+}
+
+// Open opens the blob with the given hash.
+func (s *Store) Open(hashSum string) (ReadSeekCloser, int64, error) {
+	r, length, err := s.mstore.GetForEnvironment("", hashSum)
+	if err != nil {
+		return nil, 0, err
+	}
+	return r.(ReadSeekCloser), length, nil
+}
+
+// newResourceHash returns a ResourceHash equivalent to the
+// given hashSum. It does not complain if hashSum is invalid - the
+// lower levels will bowl that out.
+func newResourceHash(hashSum string) *blobstore.ResourceHash {
+	p := hex.EncodedLen(md5.Size)
+	if len(hashSum) < p {
+		return &blobstore.ResourceHash{
+			MD5Hash: hashSum,
+		}
+	}
+	return &blobstore.ResourceHash{
+		MD5Hash:    hashSum[0:p],
+		SHA256Hash: hashSum[p:],
+	}
+}
---
+++ internal/multihash/multihash.go
@@ -0,0 +1,39 @@
+package multihash
+
+import (
+	"hash"
+)
+
+func New(h1, h2 hash.Hash) hash.Hash {
+	return &multiHash{h1, h2}
+}
+
+type multiHash struct {
+	h1, h2 hash.Hash
+}
+
+func (h *multiHash) Write(buf []byte) (int, error) {
+	h.h1.Write(buf)
+	h.h2.Write(buf)
+	return len(buf), nil
+}
+
+func (h *multiHash) Sum(buf []byte) []byte {
+	buf = h.h1.Sum(buf)
+	buf = h.h2.Sum(buf)
+	return buf
+}
+
+func (h *multiHash) Reset() {
+	h.h1.Reset()
+	h.h2.Reset()
+}
+
+func (h *multiHash) Size() int {
+	return h.h1.Size() + h.h2.Size()
+}
+
+func (h *multiHash) BlockSize() int {
+	// better: use least common multiple of both block sizes.
+	return h.h1.BlockSize()
+}
