d03fdfa822617a4e2ce1bf24f5b30f0bd208c81e
Roger Peppe <rogpeppe@gmail.com>
2014-07-22 12:03:51 +0100

    internal/multihash: add a test

--- internal/multihash/multihash.go
+++ internal/multihash/multihash.go
@@ -4,6 +4,9 @@ import (
 	"hash"
 )
 
+// New returns a hash.Hash that combines h1 and h2;
+// the resulting checksum will contain the concatenation
+// of the checksums from h1 and h2, in that order.
 func New(h1, h2 hash.Hash) hash.Hash {
 	return &multiHash{h1, h2}
 }
@@ -13,6 +16,8 @@ type multiHash struct {
 }
 
 func (h *multiHash) Write(buf []byte) (int, error) {
+	// Note: Hash.Write never returns an error.
+	// See http://golang.org/pkg/hash/#Hash
 	h.h1.Write(buf)
 	h.h2.Write(buf)
 	return len(buf), nil
