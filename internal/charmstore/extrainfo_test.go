3eea728ec17d792d72a587da6aaba12fcb256604
Francesco Banconi <francesco.banconi@canonical.com>
2014-09-05 15:22:02 +0200

    Store the machines count in the bundle extra info.

--- internal/charmstore/extrainfo_test.go
+++ internal/charmstore/extrainfo_test.go
@@ -88,7 +88,7 @@ var unitsCountTests = []struct {
 				NumUnits: 2,
 			},
 			"postgres": {
-				Charm:    "cs:utopic/haproxy-3",
+				Charm:    "cs:utopic/postgres-3",
 				NumUnits: 5,
 			},
 		},
@@ -119,11 +119,327 @@ func (s *extraInfoSuite) TestUnitsCount(c *gc.C) {
 		c.Assert(err, gc.IsNil)
 
 		// Ensure the units count is correctly included in the extra info.
-		c.Assert(doc.ExtraInfo, gc.HasLen, 1)
+		// The extra info should include both machines and units counts.
+		c.Assert(doc.ExtraInfo, gc.HasLen, 2)
 		var unitsCount int
 		err = json.Unmarshal(doc.ExtraInfo["units-count"], &unitsCount)
 		c.Assert(err, gc.IsNil)
 		c.Assert(unitsCount, gc.Equals, test.expectUnits)
+	}
+}
+
+var machinesCountTests = []struct {
+	about          string
+	data           *charm.BundleData
+	expectMachines int
+}{{
+	about: "empty bundle",
+	data:  &charm.BundleData{},
+}, {
+	about: "no machines",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:utopic/django-0",
+				NumUnits: 0,
+			},
+			"haproxy": {
+				Charm:    "cs:trusty/haproxy-0",
+				NumUnits: 0,
+			},
+		},
+	},
+}, {
+	about: "a single machine (no placement)",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:trusty/django-42",
+				NumUnits: 1,
+			},
+			"haproxy": {
+				Charm:    "cs:trusty/haproxy-47",
+				NumUnits: 0,
+			},
+		},
+	},
+	expectMachines: 1,
+}, {
+	about: "a single machine (machine placement)",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:trusty/django-42",
+				NumUnits: 1,
+				To:       []string{"1"},
+			},
+		},
+		Machines: map[string]*charm.MachineSpec{
+			"1": nil,
+		},
+	},
+	expectMachines: 1,
+}, {
+	about: "a single machine (hulk smash)",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:trusty/django-42",
+				NumUnits: 1,
+				To:       []string{"1"},
+			},
+			"haproxy": {
+				Charm:    "cs:trusty/haproxy-47",
+				NumUnits: 1,
+				To:       []string{"1"},
+			},
+		},
+		Machines: map[string]*charm.MachineSpec{
+			"1": nil,
+		},
+	},
+	expectMachines: 1,
+}, {
+	about: "a single machine (co-location)",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:trusty/django-42",
+				NumUnits: 1,
+			},
+			"haproxy": {
+				Charm:    "cs:trusty/haproxy-47",
+				NumUnits: 1,
+				To:       []string{"django/1"},
+			},
+		},
+	},
+	expectMachines: 1,
+}, {
+	about: "a single machine (containerization)",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:trusty/django-42",
+				NumUnits: 1,
+				To:       []string{"1"},
+			},
+			"haproxy": {
+				Charm:    "cs:trusty/haproxy-47",
+				NumUnits: 1,
+				To:       []string{"lxc:1"},
+			},
+			"postgres": {
+				Charm:    "cs:utopic/postgres-3",
+				NumUnits: 2,
+				To:       []string{"kvm:1"},
+			},
+		},
+		Machines: map[string]*charm.MachineSpec{
+			"1": nil,
+		},
+	},
+	expectMachines: 1,
+}, {
+	about: "multiple machines (no placement)",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:utopic/django-1",
+				NumUnits: 1,
+			},
+			"haproxy": {
+				Charm:    "cs:utopic/haproxy-2",
+				NumUnits: 2,
+			},
+			"postgres": {
+				Charm:    "cs:utopic/postgres-3",
+				NumUnits: 5,
+			},
+		},
+	},
+	expectMachines: 1 + 2 + 5,
+}, {
+	about: "multiple machines (machine placement)",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:utopic/django-1",
+				NumUnits: 2,
+				To:       []string{"1", "3"},
+			},
+			"haproxy": {
+				Charm:    "cs:utopic/haproxy-2",
+				NumUnits: 1,
+				To:       []string{"2"},
+			},
+		},
+		Machines: map[string]*charm.MachineSpec{
+			"1": nil, "2": nil, "3": nil,
+		},
+	},
+	expectMachines: 2 + 1,
+}, {
+	about: "multiple machines (hulk smash)",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:trusty/django-42",
+				NumUnits: 1,
+				To:       []string{"1"},
+			},
+			"haproxy": {
+				Charm:    "cs:trusty/haproxy-47",
+				NumUnits: 1,
+				To:       []string{"2"},
+			},
+			"postgres": {
+				Charm:    "cs:utopic/postgres-3",
+				NumUnits: 2,
+				To:       []string{"1", "2"},
+			},
+		},
+		Machines: map[string]*charm.MachineSpec{
+			"1": nil, "2": nil,
+		},
+	},
+	expectMachines: 1 + 1 + 0,
+}, {
+	about: "multiple machines (co-location)",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:trusty/django-42",
+				NumUnits: 2,
+			},
+			"haproxy": {
+				Charm:    "cs:trusty/haproxy-47",
+				NumUnits: 3,
+				To:       []string{"django/1", "django/2", "new"},
+			},
+		},
+	},
+	expectMachines: 2 + 1,
+}, {
+	about: "multiple machines (containerization)",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:trusty/django-42",
+				NumUnits: 2,
+				To:       []string{"1", "2"},
+			},
+			"haproxy": {
+				Charm:    "cs:trusty/haproxy-47",
+				NumUnits: 4,
+				To:       []string{"lxc:1", "lxc:2", "lxc:3", "lxc:3"},
+			},
+			"postgres": {
+				Charm:    "cs:utopic/postgres-3",
+				NumUnits: 1,
+				To:       []string{"kvm:2"},
+			},
+		},
+		Machines: map[string]*charm.MachineSpec{
+			"1": nil, "2": nil, "3": nil,
+		},
+	},
+	expectMachines: 2 + 0 + 1,
+}, {
+	about: "multiple machines (partial placement in a container)",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:trusty/django-42",
+				NumUnits: 1,
+				To:       []string{"1"},
+			},
+			"haproxy": {
+				Charm:    "cs:trusty/haproxy-47",
+				NumUnits: 10,
+				To:       []string{"lxc:1", "lxc:2"},
+			},
+		},
+		Machines: map[string]*charm.MachineSpec{
+			"1": nil, "2": nil,
+		},
+	},
+	expectMachines: 1 + 1,
+}, {
+	about: "multiple machines (partial placement in a new machine)",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:trusty/django-42",
+				NumUnits: 1,
+				To:       []string{"1"},
+			},
+			"haproxy": {
+				Charm:    "cs:trusty/haproxy-47",
+				NumUnits: 10,
+				To:       []string{"lxc:1", "1", "new"},
+			},
+		},
+		Machines: map[string]*charm.MachineSpec{
+			"1": nil,
+		},
+	},
+	expectMachines: 1 + 8,
+}, {
+	about: "multiple machines (partial placement with new machines)",
+	data: &charm.BundleData{
+		Services: map[string]*charm.ServiceSpec{
+			"django": {
+				Charm:    "cs:trusty/django-42",
+				NumUnits: 3,
+			},
+			"haproxy": {
+				Charm:    "cs:trusty/haproxy-47",
+				NumUnits: 6,
+				To:       []string{"new", "1", "lxc:1", "new"},
+			},
+			"postgres": {
+				Charm:    "cs:utopic/postgres-3",
+				NumUnits: 10,
+				To:       []string{"kvm:2", "lxc:django/1", "new", "new", "kvm:2"},
+			},
+		},
+		Machines: map[string]*charm.MachineSpec{
+			"1": nil, "2": nil,
+		},
+	},
+	expectMachines: 3 + 5 + 3,
+}}
+
+func (s *extraInfoSuite) TestMachinesCount(c *gc.C) {
+	entities := s.store.DB.Entities()
+	for i, test := range machinesCountTests {
+		c.Logf("test %d: %s", i, test.about)
+		url := &charm.Reference{
+			Schema:   "cs",
+			Series:   "utopic",
+			Name:     "django",
+			Revision: i,
+		}
+
+		// Add the bundle used for this test.
+		err := s.store.AddBundle(url, &extraInfoTestingBundle{
+			data: test.data,
+		}, "blobName", fakeBlobHash, fakeBlobSize)
+		c.Assert(err, gc.IsNil)
+
+		// Retrieve the bundle from the database.
+		var doc mongodoc.Entity
+		err = entities.FindId(url).Select(bson.D{{"extrainfo", 1}}).One(&doc)
+		c.Assert(err, gc.IsNil)
+
+		// Ensure the machines count is correctly included in the extra info.
+		// The extra info should include both machines and units counts.
+		c.Assert(doc.ExtraInfo, gc.HasLen, 2)
+		var machinesCount int
+		err = json.Unmarshal(doc.ExtraInfo["machines-count"], &machinesCount)
+		c.Assert(err, gc.IsNil)
+		c.Assert(machinesCount, gc.Equals, test.expectMachines)
 	}
 }
 
