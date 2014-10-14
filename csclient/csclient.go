36ac91e24c78099ecc062132c17b540a7b6f6b54
Roger Peppe <rogpeppe@gmail.com>
2014-10-14 17:29:14 +0100

    csclient: add Client.Meta method

--- csclient/csclient.go
+++ csclient/csclient.go
@@ -1,16 +1,23 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the LGPLv3, see LICENCE file for details.
+
 // The csclient package provides access to the charm store API.
 package csclient
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"reflect"
 	"strings"
+	"unicode"
 
-	"github.com/juju/errgo"
+	"gopkg.in/errgo.v1"
+	"gopkg.in/juju/charm.v4"
 
 	"github.com/juju/charmstore/params"
 )
@@ -50,6 +57,143 @@ func New(p Params) *Client {
 	return &Client{
 		params: p,
 	}
+}
+
+// Meta fetches metadata on the charm or bundle with the
+// given id. The result value provides a value
+// to be filled in with the result, which must be
+// a pointer to a struct containing members corresponding
+// to possible metadata include parameters (see http://tinyurl.com/nysdjly).
+//
+// It returns the fully qualified id of the entity.
+//
+// The name of the struct member is translated to
+// a lower case hyphen-separated form; for example,
+// ArchiveSize becomes "archive-size", and BundleMachineCount
+// becomes "bundle-machine-count", but may also
+// be specified in the field's tag
+//
+// This example will fill in the result structure with information
+// about the given id, including information on its archive
+// size (include archive-size), upload time (include archive-upload-time)
+// and digest (include extra-info/digest).
+//
+//	var result struct {
+//		ArchiveSize params.ArchiveSizeResponse
+//		ArchiveUploadTime params.ArchiveUploadTimeResponse
+//		Digest string `csclient:"extra-info/digest"`
+//	}
+//	id, err := client.Meta(id, &result)
+func (c *Client) Meta(id *charm.Reference, result interface{}) (*charm.Reference, error) {
+	if result == nil {
+		panic(fmt.Errorf("expected valid result pointer, not nil"))
+	}
+	resultv := reflect.ValueOf(result)
+	resultt := resultv.Type()
+	if resultt.Kind() != reflect.Ptr {
+		panic(fmt.Errorf("expected pointer, not %T", result))
+	}
+	resultt = resultt.Elem()
+	if resultt.Kind() != reflect.Struct {
+		panic(fmt.Errorf("expected pointer to struct, not %T", result))
+	}
+	resultv = resultv.Elem()
+
+	// At this point, resultv refers to the struct value pointed
+	// to by result, and resultt is its type.
+
+	numField := resultt.NumField()
+	includes := make([]string, 0, numField)
+
+	// results holds an entry for each field in the result value,
+	// pointing to the value for that field.
+	results := make(map[string]reflect.Value)
+	for i := 0; i < numField; i++ {
+		field := resultt.Field(i)
+		if field.PkgPath != "" {
+			// Field is private; ignore it.
+			continue
+		}
+		if field.Anonymous {
+			// At some point in the future, it might be nice to
+			// support anonymous fields, but for now the
+			// additional complexity doesn't seem worth it.
+			panic(fmt.Errorf("anonymous fields not supported"))
+		}
+		apiName := field.Tag.Get("csclient")
+		if apiName == "" {
+			apiName = hyphenate(field.Name)
+		}
+		includes = append(includes, "include="+apiName)
+		results[apiName] = resultv.FieldByName(field.Name).Addr()
+	}
+	// We unmarshal into rawResult, then unmarshal each field
+	// separately into its place in the final result value.
+	// Note that we can't use params.MetaAnyResponse because
+	// that will unpack all the values inside the Meta field,
+	// but we want to keep them raw so that we can unmarshal
+	// them ourselves.
+	var rawResult struct {
+		Id   *charm.Reference
+		Meta map[string]json.RawMessage
+	}
+	path := "/" + id.Path() + "/meta/any"
+	if len(includes) > 0 {
+		path += "?" + strings.Join(includes, "&")
+	}
+	if err := c.Get(path, &rawResult); err != nil {
+		return nil, errgo.NoteMask(err, fmt.Sprintf("cannot get %q", path), errgo.Any)
+	}
+	// Note that the server is not required to send back values
+	// for all fields. "If there is no metadata for the given meta path, the
+	// element will be omitted"
+	// See http://tinyurl.com/q5vcjpk
+	for name, r := range rawResult.Meta {
+		v, ok := results[name]
+		if !ok {
+			// The server has produced a result that we
+			// don't know about. Ignore it.
+			continue
+		}
+		// Unmarshal the raw JSON into the final struct field.
+		err := json.Unmarshal(r, v.Interface())
+		if err != nil {
+			return nil, errgo.Notef(err, "cannot unmarshal %s", name)
+		}
+	}
+	return rawResult.Id, nil
+}
+
+// hyphenate returns the hyphenated version of the given
+// field name, as specified in the Client.Meta method.
+func hyphenate(s string) string {
+	// TODO hyphenate FooHTTPBar as foo-http-bar?
+	var buf bytes.Buffer
+	var prevLower bool
+	for _, r := range s {
+		if !unicode.IsUpper(r) {
+			prevLower = true
+			buf.WriteRune(r)
+			continue
+		}
+		if prevLower {
+			buf.WriteRune('-')
+		}
+		buf.WriteRune(unicode.ToLower(r))
+		prevLower = false
+	}
+	return buf.String()
+}
+
+// Get makes a GET request to the charm store, parsing the
+// result as JSON into the given result value, which should be
+// a pointer to the expected data, but may be nil if no result is
+// desired.
+func (c *Client) Get(path string, result interface{}) error {
+	// The only reason NewRequest can fail is if the URL
+	// parsing fails, which it can't here, because it's blank.
+	req, _ := http.NewRequest("GET", "", nil)
+	return c.Do(req, path, result)
 }
 
 // Do makes an arbitrary request to the charm store.
