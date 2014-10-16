1705b0f76366372dd6a6286467386c38cf8588e8
Roger Peppe <rogpeppe@gmail.com>
2014-10-16 15:29:49 +0100

    params: rename content header

---
+++ params/params_test.go
@@ -0,0 +1,24 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the LGPLv3, see LICENCE file for details.
+
+package params_test
+
+import (
+	"net/textproto"
+
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/charmstore/params"
+)
+
+type suite struct{}
+
+var _ = gc.Suite(&suite{})
+
+func (*suite) TestContentHashHeaderCanonicalized(c *gc.C) {
+	// The header key should be canonicalized, because otherwise
+	// the actually produced header will be different from that
+	// specified.
+	canon := textproto.CanonicalMIMEHeaderKey(params.ContentHashHeader)
+	c.Assert(canon, gc.Equals, params.ContentHashHeader)
+}
