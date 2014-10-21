1595870d810d497bb42ba597123938b54dbdaefa
Roger Peppe <rogpeppe@gmail.com>
2014-10-21 13:11:36 +0100

    internal/storetesting: add YAMLEquals checker

--- internal/storetesting/checkers.go
+++ internal/storetesting/checkers.go
@@ -9,7 +9,14 @@ import (
 
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
+	"gopkg.in/yaml.v1"
 )
+
+type codecEqualChecker struct {
+	name      string
+	marshal   func(interface{}) ([]byte, error)
+	unmarshal func([]byte, interface{}) error
+}
 
 // JSONEquals defines a checker that checks whether a byte slice, when
 // unmarshaled as JSON, is equal to the given value.
@@ -17,31 +24,48 @@ import (
 // body type, we reform the expected body in JSON and
 // back to interface{}, so we can check the whole content.
 // Otherwise we lose information when unmarshaling.
-var JSONEquals = &jsonEqualChecker{
-	&gc.CheckerInfo{Name: "JSONEquals", Params: []string{"obtained", "expected"}},
+var JSONEquals = &codecEqualChecker{
+	name:      "JSONEquals",
+	marshal:   json.Marshal,
+	unmarshal: json.Unmarshal,
 }
 
-type jsonEqualChecker struct {
-	*gc.CheckerInfo
+// YAMLEquals defines a checker that checks whether a byte slice, when
+// unmarshaled as YAML, is equal to the given value.
+// Rather than unmarshaling into something of the expected
+// body type, we reform the expected body in YAML and
+// back to interface{}, so we can check the whole content.
+// Otherwise we lose information when unmarshaling.
+var YAMLEquals = &codecEqualChecker{
+	name:      "YAMLEquals",
+	marshal:   yaml.Marshal,
+	unmarshal: yaml.Unmarshal,
 }
 
-func (checker *jsonEqualChecker) Check(params []interface{}, names []string) (result bool, error string) {
+func (checker *codecEqualChecker) Info() *gc.CheckerInfo {
+	return &gc.CheckerInfo{
+		Name:   checker.name,
+		Params: []string{"obtained", "expected"},
+	}
+}
+
+func (checker *codecEqualChecker) Check(params []interface{}, names []string) (result bool, error string) {
 	gotContent, ok := params[0].([]byte)
 	if !ok {
 		return false, fmt.Sprintf("expected []byte, got %T", params[0])
 	}
 	expectContent := params[1]
-	expectContentBytes, err := json.Marshal(expectContent)
+	expectContentBytes, err := checker.marshal(expectContent)
 	if err != nil {
 		return false, fmt.Sprintf("cannot marshal expected contents: %v", err)
 	}
 	var expectContentVal interface{}
-	if err := json.Unmarshal(expectContentBytes, &expectContentVal); err != nil {
+	if err := checker.unmarshal(expectContentBytes, &expectContentVal); err != nil {
 		return false, fmt.Sprintf("cannot unmarshal expected contents: %v", err)
 	}
 
 	var gotContentVal interface{}
-	if err := json.Unmarshal(gotContent, &gotContentVal); err != nil {
+	if err := checker.unmarshal(gotContent, &gotContentVal); err != nil {
 		return false, fmt.Sprintf("cannot unmarshal obtained contents: %v; %q", err, gotContent)
 	}
 
