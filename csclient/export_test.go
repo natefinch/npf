8299b1aa76611e2d7c5efeedc231d5f8cbed57e9
Francesco Banconi <francesco.banconi@canonical.com>
2015-02-13 12:21:41 +0100

    Copy csclient package.

---
+++ csclient/export_test.go
@@ -0,0 +1,9 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the LGPLv3, see LICENCE file for details.
+
+package csclient
+
+var (
+	Hyphenate     = hyphenate
+	UploadArchive = (*Client).uploadArchive
+)
